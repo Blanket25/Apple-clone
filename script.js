b7b8ffccb0717364f36dbdd1de6ceae974651246
Blanket25 <bianca_inga@hotmail.com>
2021-06-22 16:26:33 +0200

    adding hamburger menu

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
-const btnMenu = document.getElementById("burger-menu");
+const btnMenu = document.getElementById("burger-menu-item");
+const btnMenuCross = document.getElementById("burger-menu-item-cross")
 const menu = document.getElementById("menu");
+const navBar = document.getElementById("nav-bar");
 
-function openMenu () {
-    menu.classList.toggle("unfolded-menu")
-}
+btnMenu.addEventListener('click', function() {
+    menu.style.display = "block";
+    btnMenu.style.display = "none";
+    btnMenuCross.style.display = "block";
+    navBar.style.backgroundColor = "#000";
 
-btnMenu.addEventListener('click', openMenu);
+});
+
+btnMenuCross.addEventListener('click', function() {
+    menu.style.display = "none";
+    btnMenu.style.display = "block";
+    btnMenuCross.style.display = "none";
+    navBar.style.backgroundColor = "rgba(0, 0, 0, 0.8)";
+})
